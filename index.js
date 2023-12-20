585f1f2db92db6b897973f36985472b94bfd98b7
anderutan <52438782+anderutan@users.noreply.github.com>
2023-12-20 15:44:49 +0800

    add reset score button

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let plusOneGuest = document.querySelector('.plus-1-g');
 let plusTwoGuest = document.querySelector('.plus-2-g');
 let plusThreeGuest = document.querySelector('.plus-3-g');
 let guestScore = document.querySelector('.guest-score');
+let resetBtn = document.querySelector('.reset');
 
 let gScore = 0;
 let hScore = 0;
@@ -61,4 +62,27 @@ plusTwoGuest.addEventListener('click', () => {
 plusThreeGuest.addEventListener('click', () => {
   addGuestScore(3);
   dimmer();
+});
+
+function reset() {
+  hScore = 0;
+  homeScore.textContent = 0;
+  gScore = 0;
+  guestScore.textContent = 0;
+  dimmer();
+}
+
+resetBtn.addEventListener('click', () => {
+  let ask = prompt(
+    'Are you sure to reset the score? [Yes/No]',
+    'Yes'
+  ).toLowerCase();
+  if (ask === 'yes' || ask === 'y') {
+    reset();
+  } else if (ask === 'no' || ask === 'n') {
+    return;
+  } else {
+    alert('Please reply Yes or No!');
+    return;
+  }
 });
